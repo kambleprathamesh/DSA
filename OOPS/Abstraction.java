053cb3befc11532684a3075e30b864279b008077
prathmesh <prathameshk990@gmail.com>
2024-09-25 09:58:08 +0530

    Abstraction

---
+++ OOPS/Abstraction.java
@@ -0,0 +1,46 @@
+public class Abstraction {
+    public static void main(String args[]) {
+        Horse h = new Horse();
+        h.eat();
+        System.out.println(h.setColor("pink"));
+
+    }
+}
+
+abstract class Animal {
+    String color = "Brown";
+
+    abstract void eat();
+
+    Animal() {
+        System.out.println("Animal Constructor called...");
+    }
+}
+
+class Horse extends Animal {
+
+    Horse() {
+        System.out.println("Horse Constructor called...");
+    }
+
+    String setColor(String color) {
+        return this.color = color;
+    }
+
+    void eat() {
+        System.out.println("Horse  eat function called...");
+
+    }
+
+}
+
+class Mustang {
+    Mustang() {
+        System.out.println("Mustang Constructor called...");
+    }
+
+    void eat() {
+        System.out.println("Mustang  eat function called...");
+
+    }
+}
