15acc7516b1aa9ac5a152dad610413fdabafbac9
prathmesh <prathameshk990@gmail.com>
2024-09-30 00:25:21 +0530

    Smallest Two d Array

--- Array/MinMax.java
+++ Array/MinMax.java
@@ -1,18 +1,35 @@
 public class MinMax {
     public static void main(String[] args) {
         int[] arr = { 5, 3, 2, 6, 9, 7 };
-        System.out.println(min(arr));
+        int[][] arr1 = {
+                { 5, 4, 8 },
+                { 9, 566, 6 },
+                { 10, 100, 7 }
+        };
+        System.out.println(minTDArray(arr1));
     }
 
-    static int min(int[] arr) {
+    static int minArray(int[] arr) {
         int min = arr[0];
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (min > arr[i + 1]) {
-                min = arr[i + 1];
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
             }
         }
         return min;
     }
 
+    static int minTDArray(int[][] arr) {
+        int min = arr[0][0];
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (min > arr[row][col]) {
+                    min = arr[row][col];
+                }
+            }
+        }
+        return min;
+    }
 }
