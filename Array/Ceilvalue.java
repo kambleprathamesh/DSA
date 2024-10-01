cf5d60255b477f3c6ca0dd3a8fb9b6e56f22145e
prathmesh <prathameshk990@gmail.com>
2024-10-02 01:17:06 +0530

    added Order Agnostic

--- Array/Ceilvalue.java
+++ Array/Ceilvalue.java
@@ -1,3 +1,52 @@
 public class Ceilvalue {
+    public static void main(String[] args) {
+        // int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int arr[] = { 6, 5, 4, 3, 2, 1 };
+        int number = FloorValue(arr, 6);
+        System.out.println(number);
+    }
+
+    static int Ceilvalue1(int[] arr, int key) {
+        // apply binary search
+
+        int start = 0;
+        int end = arr.length - 1;
+        // step1-find mid
+        int mid = start + (end - start) / 2;
+
+        // compare key and target
+        while (start <= end) {
+            if (arr[mid] == key) {
+                System.out.println("key Found at Index : " + mid);
+                return key;
+            } else if (arr[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            // accordingly increase/decrease the Value
+        }
+        return start;
+    }
+
+    static int FloorValue(int[] arr, int key) {
+        // declare the varaible
+        // finf the middle
+        // compare
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = start + (end - start) / 2;
+        while (start <= end) {
+            if (arr[mid] > key) {
+                end = mid - 1;
+            } else if (arr[mid] < key) {
+                start = mid + 1;
+            } else if (arr[mid] == key) {
+                return arr[end];
+            }
+
+        }
+        return end;
+    }
 
 }
