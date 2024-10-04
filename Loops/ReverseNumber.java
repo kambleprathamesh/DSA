99aa2f6f2f357de0ef85cc51b67d2df29a95b70f
prathmesh <prathameshk990@gmail.com>
2024-10-04 22:18:15 +0530

    Reverse String

---
+++ Loops/ReverseNumber.java
@@ -0,0 +1,18 @@
+package Loops;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        reverseNum(123456789);
+    }
+
+    static void reverseNum(int num) {
+        int rev = 0;
+        // find the last digit
+
+        while (num > 0) {
+            int lastDigit = num % 10;
+            rev = rev * 10 + lastDigit;
+            num = num / 10;
+        }
+        System.out.println(rev);
+    }
+}
