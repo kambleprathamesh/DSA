754e2ef9ba085a7a490c9b9e09eedee96ec9bf10
prathmesh <prathameshk990@gmail.com>
2024-10-07 02:57:11 +0530

    Diamond

--- Patterns/Pattern3.java
+++ Patterns/Pattern3.java
@@ -1,6 +1,6 @@
 public class Pattern3 {
     public static void main(String[] args) {
-        Pattern28(5);
+        pattern28(5);
     }
 
     // pattern1
@@ -42,18 +42,35 @@ public class Pattern3 {
     // }
     // }
 
-    static void Pattern28(int n) {
-        for (int row = 0; row <2 * n; row++) {
+    static void pattern28(int n) {
+        for (int row = 0; row < 2 * n; row++) {
             int totalCols = row > n ? 2 * n - row : row;
             int noOfSpaces = n - totalCols;
             for (int i = 0; i < noOfSpaces; i++) {
                 System.out.print(" ");
             }
             for (int col = 0; col < totalCols; col++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
 
         }
     }
+
+    // static void pattern28(int n) {
+    // for (int row = 0; row < 2 * n; row++) {
+    // int totalCols = row > n ? 2 * n - row : row; // Correct condition here
+
+    // int noOfSpaces = n - totalCols;
+    // for (int s = 0; s < noOfSpaces; s++) {
+    // System.out.print(" ");
+    // }
+
+    // for (int col = 0; col < totalCols; col++) {
+    // System.out.print("* "); // Adding space between stars
+    // }
+    // System.out.println();
+    // }
+    // }
+
 }
