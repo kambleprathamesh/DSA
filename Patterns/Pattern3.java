0cc0622c456381ae47135ec0840bb5427cdbe197
prathmesh <prathameshk990@gmail.com>
2024-10-06 19:53:14 +0530

    pattern28

--- Patterns/Pattern3.java
+++ Patterns/Pattern3.java
@@ -1,6 +1,6 @@
 public class Pattern3 {
     public static void main(String[] args) {
-        Pattern2(5);
+        Pattern28(5);
     }
 
     // pattern1
@@ -31,11 +31,25 @@ public class Pattern3 {
     // **
     // *
 
-    static void Pattern2(int n) {
-        for (int row = 0; row <= 2 * n; row++) {
-            int totalCols = row > n ? 2 * n - row : row;
+    // static void Pattern2(int n) {
+    // for (int row = 0; row <= 2 * n; row++) {
+    // int totalCols = row > n ? 2 * n - row : row;
+    // for (int col = 0; col <= totalCols; col++) {
+    // System.out.print("*");
+    // }
+    // System.out.println();
 
-            for (int col = 0; col <= totalCols; col++) {
+    // }
+    // }
+
+    static void Pattern28(int n) {
+        for (int row = 0; row <2 * n; row++) {
+            int totalCols = row > n ? 2 * n - row : row;
+            int noOfSpaces = n - totalCols;
+            for (int i = 0; i < noOfSpaces; i++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < totalCols; col++) {
                 System.out.print("*");
             }
             System.out.println();
