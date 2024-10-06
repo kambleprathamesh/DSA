ecbce77578078715bd6c2523726181a1c2ec0955
prathmesh <prathameshk990@gmail.com>
2024-10-06 19:08:40 +0530

    pattern2

--- Patterns/Pattern3.java
+++ Patterns/Pattern3.java
@@ -1,14 +1,50 @@
 public class Pattern3 {
     public static void main(String[] args) {
-        pattern1(5);
+        pattern2(5);
     }
 
-    static void pattern1(int n) {
-        for (int row = 1; row <= n; row++) {
-            for (int col = 1; col <=n - row+1; col++)
-                System.out.print("*");
+    // pattern1
+    // ******* /
+    // ****** /
+    // ****
+    // ***
+    // **
+    // *
 
-            System.out.println();
+    // static void pattern1(int n) {
+    // for (int row = 1; row <= n; row++) {
+    // for (int col = 1; col <=n - row+1; col++)
+    // System.out.print("*");
+
+    // System.out.println();
+    // }
+    // }
+
+    // pattern2
+    // *
+    // **
+    // ***
+    // ****
+    // ******
+    // ****
+    // ***
+    // **
+    // *
+
+    static void pattern2(int n) {
+        for (int row = 1; row < 2 * n; row++) {
+            if (row > n) {
+                for (int col = 1; col <= n - (row - n); col++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            } else {
+                for (int col = 0; col <=row; col++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
         }
     }
+
 }
