58baf1c7a59064fdad93bce18148fedbe890a383
prathmesh <prathameshk990@gmail.com>
2024-10-12 15:11:12 +0530

    HollowRectangles

--- Patterns/HollowRectangles.java
+++ Patterns/HollowRectangles.java
@@ -1,19 +1,19 @@
-// public class HollowRectangles {
+public class HollowRectangles {
 
-//     public static void HollowRec(int totRows, int totCols) {
-//         for (int i = 1; i <= totRows; i++) {
-//             for (int j = 1; j <= totCols; j++) {
-//                 if (i == 1 || i == totRows || j == 1 || j == totCols) {
-//                     System.out.print("*" );
-//                 } else {
-//                     System.out.print(" ");
-//                 }
-//             }
-//             System.out.println();
-//         }
-//     }
+    public static void HollowRec(int totRows, int totCols) {
+        for (int i = 1; i <= totRows; i++) {
+            for (int j = 1; j <= totCols; j++) {
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    System.out.print("*" );
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
-//     public static void main(String[] args) {
-//         HollowRec(4, 5);
-//     }
-// }
+    public static void main(String[] args) {
+        HollowRec(4, 5);
+    }
+}
