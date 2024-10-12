f042c1ddfc9ae5a9741a0fb8591a2415747b53c3
prathmesh <prathameshk990@gmail.com>
2024-10-12 16:28:44 +0530

    Inverted floyeds traingle

--- Patterns/Ftriangle.java
+++ Patterns/Ftriangle.java
@@ -10,7 +10,20 @@ public class Ftriangle {
         }
     }
 
+    static void Ift(int n) {
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i+1; j++) {
+                System.out.print(count );
+                count++;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         ft(5);
+        System.out.println();
+        Ift(5);
     }
 }
