0e6519107ed265f5987aaf534f898f347e0ef993
prathmesh <prathameshk990@gmail.com>
2024-10-12 15:10:42 +0530

    InvertedHalfPyramid

--- Patterns/InvertedHalfPyramid.java
+++ Patterns/InvertedHalfPyramid.java
@@ -1,22 +1,22 @@
-// public class InvertedHalfPyramid {
-//     static void invereted(int n) {
-//         for (int i = 1; i <= n; i++) {
+public class InvertedHalfPyramid {
+    static void invereted(int n) {
+        for (int i = 1; i <= n; i++) {
 
-//             // spaces
-//             for (int j = 1; j <= n - i; j++) {
-//                 System.out.print(" ");
-//             }
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
 
-//             // stars
-//             for (int star = 1; star <= i; star++) {
-//                 System.out.print("*");
-//             }
-//             System.out.println();
+            // stars
+            for (int star = 1; star <= i; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
 
-//         }
-//     }
+        }
+    }
 
-//     public static void main(String[] args) {
-//         invereted(5);
-//     }
-// }
+    public static void main(String[] args) {
+        invereted(5);
+    }
+}
